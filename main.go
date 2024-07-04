73439e5ab48790ea5d3455a2ebc860247e644577
filmonwmr <filmon.seare@windmyroof.com>
2024-07-04 16:42:25 +0200

    Fix JWT authentication and add URL shortening functionality

--- main.go
+++ main.go
@@ -3,18 +3,15 @@ package main
 import (
     "url-shortener/config"
     "url-shortener/routes"
-     ginSwagger "github.com/swaggo/gin-swagger"
-    "github.com/swaggo/files"
+    swaggerFiles "github.com/swaggo/files"
+    ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func main() {
-    
     config.ConnectDatabase()
-
     r := routes.SetupRouter()
     r.Static("/docs", "./docs")
-    url := ginSwagger.URL("/docs/swagger.json") // The url pointing to API definition
+    url := ginSwagger.URL("/docs/swagger.json")
     r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
-
     r.Run(":8080")
 }
